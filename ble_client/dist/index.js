13a0c9e7e56316ed2960e101433af7f977df6d72
markchen0308 <simonchen0308@gmail.com>
2018-08-06 12:37:25 +0800

    modify index

--- ble_client/dist/index.js
+++ ble_client/dist/index.js
@@ -11,6 +11,7 @@ let BLE_TX_UUID = '6e400002b5a3f393e0a9e50e24dcca9e'; //read from peripheral
 let rx_TH_sensor = ''; //temperature and humdity sensor data
 let rx_PIR_sensor = ''; //PIT sensor
 let time; //record time
+let bleStatus;
 //read all sensor
 function readSensor() {
     //execution per 1 second
@@ -25,30 +26,36 @@ function readSensor() {
         rx_PIR_sensor = ble.readPIRSensorData();
         if (rx_PIR_sensor != '') {
             console.log(time + ' PIR sensor =' + rx_PIR_sensor);
+            //control light function here according to PIR value 
         }
     }, 1000);
 }
 //system start 
-function SystemStart() {
+function systemStart() {
     console.log("Start to run IOT System.");
-    //turn up ble
+    //turn on ble
     CP.exec(BLE_START_CMD, (err, stdout, stderr) => {
         if (err) {
-            console.log('ble start error:' + stderr);
+            bleStatus = false;
+            console.log('Can not turn on ble:' + stderr);
         }
         else {
-            console.log('ble start ok');
+            bleStatus = true;
+            console.log('ble is turned on');
         }
     });
-    //execution after 1 second 
-    setTimeout(() => {
-        ble = new BleClientControl_1.BluetoothController(BLE_DEVICE_UUID, BLE_RX_UUID, BLE_TX_UUID);
-    }, 1000);
-    //execution after 5 second 
-    setTimeout(() => {
-        readSensor();
-    }, 5000);
+    if (bleStatus) //if ble is turned on 
+     {
+        //execution after 1 second 
+        setTimeout(() => {
+            ble = new BleClientControl_1.BluetoothController(BLE_DEVICE_UUID, BLE_RX_UUID, BLE_TX_UUID);
+        }, 1000);
+        //execution after 5 second 
+        setTimeout(() => {
+            readSensor();
+        }, 5000);
+    }
 }
 ;
 //start all system
-SystemStart();
+systemStart();
--- ble_client/src/index.ts
+++ ble_client/src/index.ts
@@ -11,52 +11,61 @@ let BLE_TX_UUID: string = '6e400002b5a3f393e0a9e50e24dcca9e';//read from periphe
 let rx_TH_sensor: string = '';//temperature and humdity sensor data
 let rx_PIR_sensor: string = '';//PIT sensor
 
-let time:String;//record time
+let time: String;//record time
+let bleStatus: boolean;
 
 //read all sensor
 function readSensor() {
     //execution per 1 second
     setInterval(() => {
-        time=(new Date()).toLocaleString();//record time
+        time = (new Date()).toLocaleString();//record time
         //read temperature and humidity sensor data
         rx_TH_sensor = ble.readTHSensorData();
         if (rx_TH_sensor != '') {
-            console.log(time+ ' TH sensor =' + rx_TH_sensor);
+            console.log(time + ' TH sensor =' + rx_TH_sensor);
         }
 
         //read PIR sensor data
         rx_PIR_sensor = ble.readPIRSensorData();
         if (rx_PIR_sensor != '') {
-            console.log(time+' PIR sensor =' + rx_PIR_sensor);
+            console.log(time + ' PIR sensor =' + rx_PIR_sensor);
+            //control light function here according to PIR value 
         }
     }, 1000);
 }
 
 //system start 
-function SystemStart() {
+function systemStart() {
     console.log("Start to run IOT System.");
-    //turn up ble
+
+    //turn on ble
     CP.exec(BLE_START_CMD, (err: Error, stdout: string, stderr: string) => {
         if (err) {
-            console.log('ble start error:' + stderr);
+            bleStatus = false;
+            console.log('Can not turn on ble:' + stderr);
         }
         else {
-            console.log('ble start ok');
+            bleStatus = true;
+            console.log('ble is turned on');
         }
     });
 
-    //execution after 1 second 
-    setTimeout(() => {
-        ble = new BluetoothController(BLE_DEVICE_UUID, BLE_RX_UUID, BLE_TX_UUID);
-    }, 1000);
+    if (bleStatus)//if ble is turned on 
+    {
+        //execution after 1 second 
+        setTimeout(() => {
+            ble = new BluetoothController(BLE_DEVICE_UUID, BLE_RX_UUID, BLE_TX_UUID);
+        }, 1000);
 
-    //execution after 5 second 
-    setTimeout(() => {
-        readSensor();
-    }, 5000);
+        //execution after 5 second 
+        setTimeout(() => {
+            readSensor();
+        }, 5000);
+    }
+
 };
 
 
 //start all system
-SystemStart();
+systemStart();
 
